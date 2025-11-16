099c17fe5a6548d930af551d61309f6ade871826
Kuria300 <eugenekuria66@gmail.com>
2025-11-16 22:25:15 +0300

    made chnages to garden managemnet file

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,41 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80){
+  console.log('Watering on')
+
+}else{
+    console.log('watering off')
+}
+
+
+if (timeOfDay == 'evening' || timeOfDay == 'night'){
+    console.log('Lights on')
+}else{
+    console.log('Lights off')
+}
+
+
+// for(let i= 5; soilMoisture < 40; i++){
+//     console.log(soilMoisture)
+
+
+//     if( soilMoisture + i >40){
+//        i= 40 - soilMoisture
+//     }
+//     soilMoisture += i
+//     i++
+
+
+// }
+
+ let i =5
+while(soilMoisture < 40){
+   console.log(soilMoisture)
+   soilMoisture += i
+
+}
+if (soilMoisture == 40){
+    console.log(soilMoisture)
+   }
